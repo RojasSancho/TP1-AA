ee6c56385b33c012d792f4e37266b09dadc5c946
RojasSancho <hermes.rojassancho@ucr.ac.cr>
2022-10-15 17:45:24 -0600

    M

---
+++ colaArregloCircular.h
@@ -0,0 +1,21 @@
+#ifndef COLAARREGLOCIRCULAR_H
+#define COLAARREGLOCIRCULAR_H
+
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class ColaArregloCircular {
+
+    int frente;
+    int final;
+    int contador;
+    int M; //Tamaño maximo de la cola
+
+    vector<int> numeros;
+};
+
+#endif
+
+
