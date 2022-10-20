dbcc28005b39d37aa30d70d1810efdbdb898f59d
RojasSancho <hermes.rojassancho@ucr.ac.cr>
2022-10-19 19:00:22 -0600

    programa pruebas, terminado cola menu

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "colaArregloCircular.h"
 #include "arbolHMIHDContador.h"
+#include "listaIndexada.h"
 //#include "arbolHMIHDConPunteros.h"
 //#include "arbolHMIHDUltimoApuntandoPadre.h"
 #include <iostream>
@@ -8,46 +9,105 @@ using namespace std;
 
 int main()
 {
-    /*
-    ColaArregloCircular *cola = new ColaArregloCircular();
-    cola->Iniciar(5);
-    cola->Encolar(5);
-    cola->Encolar(20);
-    cola->Encolar(3);
-    cola->Encolar(3);
-    cola->Encolar(6);
-    
-    cola->Desencolar();
-    cola->Desencolar();
-    
-    cola->Encolar(7);
-    cola->Encolar(8);
-    cola->Encolar(9);
-    
-    
-    cout << cola->numeros[0] << "\n";
-    cout << cola->numeros[1] << "\n";
-    cout << cola->numeros[2] << "\n";
-    cout << cola->numeros[3] << "\n";
-    cout << cola->numeros[4] << "\n";
-    cout << cola->numeros[5] << "\n";
-    cout << cola->numeros[7] << "\n";
-    */
-    
-    ArbolHMIHDContador *arbol = new ArbolHMIHDContador();
-    //arbolHMIHDConPunteros *arbol = new arbolHMIHDConPunteros();
-    //arbolHMIHDUltimoApuntandoPadre *arbol = new arbolHMIHDUltimoApuntandoPadre();
-    arbol->Iniciar();
-    arbol->PonerRaiz(1);
-    arbol->AgregarHijo(arbol->Raiz(), 2);
-    arbol->AgregarHijoMasDerecho(arbol->Raiz(), 3);
+    bool terminar = false;
 
-    //Nodo *nodo = arbol->Raiz()->HijoMasIzquierdo()->HermanoDerecho();
-    //Nodo *nodo2 = arbol->AgregarHijo(nodo, 4);
-    
-    //Nodo *padre = arbol->Padre(nodo2);
-    
+    while(!terminar)
+    {
+        int opcionMenuPrin = 0;
 
+        cout<< "\n\nBienvenido! Este programa esta hecho para probar diferentes modelos matematicos como lo son la Cola, la Lista Indexada y el Arbol n-ario.\n"; 
+        cout<<"Elija entre las siguiente opciones digitando uno de los numeros:\n";
+        cout<<"\n1. Cola\n";
+        cout<<"2. Lista Indexada\n";
+        cout<<"3. Arbol n-ario\n";
+        cout<<"\n4. Salir del programa\n";
+        cin>>opcionMenuPrin;
 
+        if(opcionMenuPrin == 4)
+        {
+            cout<<"\nHasta pronto!\n";
+            terminar = true;
+        }
 
+        switch (opcionMenuPrin)
+        {
+            
+        case 1: //Modelo Cola
+        {
+            bool salirAMenuPrin = false;
+            ColaArregloCircular *colaUsuario = new ColaArregloCircular();
+            while(!salirAMenuPrin) 
+            {
+                int opcionCola = 0;
+                cout<<"\n\nCOLA\nDigite alguna de las opciones para utilizar el operador basico o 6 para volver al menu principal:\n\n";
+                cout<<"1. Iniciar\n";
+                cout<<"2. Destruir\n";
+                cout<<"3. Encolar\n";
+                cout<<"4. Desencolar\n";
+                cout<<"5. NumElem (Numero de elementos en la cola)\n";
+                cout<<"\n6. Salir al menu principal\n";
+                cin>>opcionCola;
+
+                if(opcionCola == 6)
+                {
+                    salirAMenuPrin = true;
+                    delete colaUsuario;
+                }
+
+                switch (opcionCola)
+                {
+                case 1:
+                {
+                    int M = 0;
+                    cout<<"\nIngrese el numero maximo de elementos (M) podra tener la cola:";
+                    cin>>M;
+                    colaUsuario->Iniciar(M);
+                    cout<<"\nCola iniciada!";
+                    break;
+                }
+                case 2:
+                {    
+                    delete colaUsuario;
+                    break;
+                }
+                case 3:
+                {    
+                    int nuevoEncolado = 0;
+                    cout<<"\nDigite el entero que desea encolar:";
+                    cin>>nuevoEncolado;
+                    colaUsuario->Encolar(nuevoEncolado);
+                    cout<<"\nEntero encolado";
+                    break;
+                }    
+                case 4:
+                {    
+                    int desencolado = colaUsuario->Desencolar();
+                    cout<<"\nEl entero que se acaba de desencolar es: " << desencolado;
+
+                    break;
+                }     
+                case 5:
+                {  
+                    int numElem = colaUsuario->NumElem();  
+                    cout<<"\nEl numero actual de elementos de la cola es: " << numElem;
+                    break;
+                }        
+                default:
+                    break;
+                }
+            }
+            break;
+        }
+        case 2: //Modelo Lista
+
+            break;
+
+        case 3: //Modelo Arbol n-ario
+            break; 
+
+        default:
+            break;
+        }
+
+    }  
 }
