7bd191afffa128ae36fe22a6a9f72a79ee01537c
RojasSancho <hermes.rojassancho@ucr.ac.cr>
2022-10-19 22:09:21 -0600

    envio preliminar

--- src/main.cpp
+++ src/main.cpp
@@ -10,9 +10,9 @@ Sánchez Cárdenas Carlos David C17235
 #include "listaIndexada.h"
 
 #include "arbolHMIHDContador.h"
-#include "arbolHMIHDconPunteros.h"
-#include "./arbolArregloConSenaladorPadre/arbolArregloConSenaladorPadre.h"
-#include "arbolHMIHDUltimoApuntandoPadre.h"
+//#include "arbolHMIHDconPunteros.h"
+//#include "./arbolArregloConSenaladorPadre/arbolArregloConSenaladorPadre.h"
+//#include "arbolHMIHDUltimoApuntandoPadre.h"
 
 #include <iostream>
 
