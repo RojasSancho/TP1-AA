9f17d17777218e9baa1d97269e35cd18407fb36e
carlos <carlosdav03@gmail.com>
2022-10-31 16:07:07 -0600

    nodoArbol.h

---
+++ src/nodoArbol.h
@@ -0,0 +1,18 @@
+#ifndef NODO_H
+#define NODO_H
+
+using namespace std;
+
+class NodoArbol
+{
+    int etiqueta; //ESTE
+
+    public:
+
+    Nodo(); //ESTE
+    void ModificarEtiqueta(int etiqueta); //ESTE
+    int Etiqueta(); //ESTE
+
+};  
+
+#endif
