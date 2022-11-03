bf1a40806bb49d7fb6f237572eada97983d40ce4
carlos <carlosdav03@gmail.com>
2022-11-03 17:05:03 -0600

    .

--- v2/src/arbol5/nodo5.cpp
+++ v2/src/arbol5/nodo5.cpp
@@ -7,27 +7,66 @@ Nodo5::Nodo5(){
 
 }
 
-Nodo5::Nodo5(Nodo5 &elemento){
-    this->actual = new Nodo5(elemento);
+Nodo5::Nodo5(int etiqueta){
+    this->etiqueta = etiqueta;
 }
 
-Nodo5::Nodo5(Nodo5 *nodo){
+// Nodo5::Nodo5(Nodo5 *nodo){
 
+// }
+
+Nodo5::Nodo5(Nodo5 *hijoMasIzquierdo, Nodo5 *hermanoDerecho, int etiqueta)
+{
+    this->hijoMasIzquierdo = hijoMasIzquierdo;
+    this->hermanoDerecho = hermanoDerecho;
+    this->etiqueta = etiqueta;
+    
 }
 
 Nodo5::~Nodo5(){
-    if (actual != nullptr){
-        delete this->actual;
-    }
+    // if (actual != nullptr){
+    //     delete this->actual;
+    // }
+
 }
 
-Nodo5 *Nodo5::get(){
-    Nodo5 valorNodo = &this->actual;
-    return valorNodo;
+int *Nodo5::get(){
+    //Nodo5 valorNodo = &this->actual;
+    return this->etiqueta;
 }
 
-Nodo5 Nodo5::modificarContenido(Nodo5 &elemento){
-    delete this->actual;
-    this->actual = new Nodo5(elemento);
+void Nodo5::modificarContenido(int etiqueta){
+    // delete this->actual;
+    // this->actual = new Nodo5(elemento);
+    this->etiqueta = etiqueta;
 }
 
+Nodo5 *Nodo5::HijoMasIzquierdo() 
+{
+    return this->hijoMasIzquierdo;
+}
+
+Nodo5 *Nodo5::HermanoDerecho()
+{
+    return this->hermanoDerecho;
+}
+
+Nodo5 *Nodo5::Padre()
+{
+    return this->padre;
+}
+
+void Nodo5::SetearHMI(Nodo5* HMI)
+{
+    this->hijoMasIzquierdo = HMI;
+}
+
+void Nodo5::SetearHD(Nodo5* HD)
+{
+    this->hermanoDerecho = HD;
+}
+
+void Nodo5::SetearPadre(Nodo5* padre)
+{
+    this->padre = padre;
+} 
--- v2/src/arbol5/nodo5.h
+++ v2/src/arbol5/nodo5.h
@@ -15,17 +15,23 @@ class Nodo5{
 
     Nodo5* hijoMasIzquierdo = nullptr;
     Nodo5* hermanoDerecho = nullptr;
-    Nodo5 *actual = nullptr;
+    // elemento Nodo5 *actual = nullptr;
     Nodo5* padre = nullptr;
+    int etiqueta;
 
     public:
 
     Nodo5();
-    Nodo5(Nodo5 *nodo);
-    Nodo5(Nodo5 *elemento)
+    //Nodo5(Nodo5 *nodo);
+    Nodo5(int etiqueta);
+    Nodo5(Nodo5 *hijoMasIzquierdo, Nodo5 *hermanoDerecho, int etiqueta);
     ~Nodo5();
     Nodo5 *get();
     Nodo5 modificarContenido(Nodo5 elemento);
+
+    void SetearHMI(Nodo5* HMI); //Modificar Hijo Mas Izquierdo
+    void SetearHD(Nodo5* HD); //Modificar HermanoDerecho
+    void SetearPadre(Nodo5* padre);  
 
 };
 
