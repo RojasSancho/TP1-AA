20d4a0b18efc095dffe1c488acc61aac041d630c
carlos <carlosdav03@gmail.com>
2022-11-08 12:22:41 -0600

    nodo arreglo con senalador al padre

--- v2/src/arbol1/nodo1.h
+++ v2/src/arbol1/nodo1.h
@@ -7,9 +7,24 @@ using namespace std;
 
 class Nodo1  {
     private:
+    
+    int indice;
+    int padre;
+    int valor;
 
     public:
 
+    Nodo1();
+    Nodo1(int valor,Nodo1 padre);
+    ~Nodo1();
+
+    void setIndice();
+    void setPadre();
+    void setValor();
+
+    int getIndice();
+    int getPadre();
+    int getValor();
 };
 
 typedef Nodo1 Nodo;
